b08e36c649deee3239b54f4ec57e32e2904cd1fe
Darren Clarke <darren@redaranj.com>
2025-07-10 09:40:29 +0200

    Optimize imports

--- example/app/src/main/java/org/contentauth/c2paexample/C2PATestScreen.kt
+++ example/app/src/main/java/org/contentauth/c2paexample/C2PATestScreen.kt
@@ -1,25 +1,49 @@
 package org.contentauth.c2paexample
 
 import android.content.Context
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import org.contentauth.c2pa.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.contentauth.c2pa.Builder
+import org.contentauth.c2pa.C2PA
+import org.contentauth.c2pa.C2PAError
+import org.contentauth.c2pa.C2paSeekMode
+import org.contentauth.c2pa.CallbackStream
+import org.contentauth.c2pa.Reader
+import org.contentauth.c2pa.Signer
+import org.contentauth.c2pa.SignerInfo
+import org.contentauth.c2pa.SigningAlgorithm
+import org.contentauth.c2pa.Stream
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.security.Signature
-import java.util.Base64
 
 data class TestResult(
     val name: String,
